d312da369d83b285b64da16c05a414d623fcf64b
John Vresilovic <johnvresilovic@protonmail.com>
2023-01-08 12:44:54 -0500

    works

---
+++ maker.js
@@ -0,0 +1,45 @@
+//assign values to the key variables
+let letters = []
+let words = []
+
+//DOM manipulation
+const letterBox = document.getElementById("letter-box");
+
+const wordBox = document.getElementById("word-box");
+
+const letterInput = document.getElementById("letter")
+
+const wordInput = document.getElementById("word")
+
+//event listeners
+letterInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault()
+        document.getElementById("letter-button").click()
+        document.getElementById("letter").value = ""
+    }
+})
+
+wordInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault()
+        document.getElementById("word-button").click()
+        document.getElementById("word").value = ""
+    }
+})
+
+//make letter array
+function makeLetterArray() {
+    let newLetter = document.getElementById("letter").value
+    letters.push(newLetter)
+    letterBox.innerText = `${letters}  `
+    console.log(letters)
+}
+
+//make word array
+function makeWordArray() {
+    let newWord = document.getElementById("word").value
+    words.push(newWord)
+    wordBox.innerText = `${words} `
+    console.log(words)
+}
