487a040a0634139c2e09284cad0272ad1f0c8f55
John Vresilovic <johnvresilovic@protonmail.com>
2023-02-07 12:49:09 -0500

    display work

--- maker.js
+++ maker.js
@@ -1,7 +1,7 @@
 //assign values to the key variables
-let letters = []
-let words = []
-let pangrams = []
+let letters = [];
+let words = [];
+let pangrams = [];
 
 //DOM manipulation
 const lettersBox = document.getElementById("letters-box");
@@ -14,60 +14,68 @@ const nameBox = document.getElementById("puzzle-name-box");
 
 const nameInput = document.getElementById("puzzle-name");
 
-const letterInput = document.getElementById("letter")
+const letterInput = document.getElementById("letter");
 
-const wordInput = document.getElementById("word")
+const wordInput = document.getElementById("word");
 
 const pangramInput = document.getElementById("pangrams");
 
 //event listeners
-letterInput.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault()
-        document.getElementById("letter-button").click()
-        document.getElementById("letter").value = ""
-    }
-})
+letterInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("letter-button").click();
+    document.getElementById("letter").value = "";
+  }
+});
 
-wordInput.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault()
-        document.getElementById("word-button").click()
-        document.getElementById("word").value = ""
-    }
-})
+wordInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("word-button").click();
+    document.getElementById("word").value = "";
+  }
+});
 
-pangramsInput.addEventListener("keypress", function(event) {
+pangramsInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("pangrams-button").click();
+    document.getElementById("pangrams").value = "";
+  }
+});
+
+nameInput.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
-        event.preventDefault()
-        document.getElementById("pangrams-button").click()
-        document.getElementById("pangrams").value = ""
+      event.preventDefault();
+      document.getElementById("name-button").click();
+      document.getElementById("puzzle-name").value = "";
     }
-})
+  });
 
 //make letter array
 function makeLetterArray() {
-    let newLetter = document.getElementById("letter").value
-    letters.push(newLetter)
-    lettersBox.innerText = `[${letters},]`
+  let newLetter = document.getElementById("letter").value;
+  letters.push(newLetter);
+  lettersBox.innerText = `[${letters},]`;
 }
 
 //make word array
 function makeWordArray() {
-    let newWord = document.getElementById("word").value
-    words.push(newWord)
-    wordsBox.innerText = `[${words},]`
+  let newWord = document.getElementById("word").value;
+  words.push(newWord);
+  wordsBox.innerText = `[${words},]`;
 }
 
 //make pangrams array
 function makePangramsArray() {
-    let newPangram = document.getElementById("pangrams").value
-    pangrams.push(newPangram)
-    pangramsBox.innerText = `[${pangrams},]`
+  let newPangram = document.getElementById("pangrams").value;
+  pangrams.push(newPangram);
+  pangramsBox.innerText = `[${pangrams},]`;
 }
 
 //display puzzle name
 function displayPuzzleName() {
-    let puzzleName = document.getElementById("puzzle-name").value
-    nameBox.innerText = `${puzzleName}`
+  let puzzleName = document.getElementById("puzzle-name").value;
+  nameBox.innerText = `${puzzleName}`;
 }
